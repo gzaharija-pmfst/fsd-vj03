8d22e0c1294c2ec8baf6bd287cc5459b0f5ba560
gzaharija <goran.zaharija@gmail.com>
2020-10-12 01:59:08 +0200

    Nova komponenta

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const poruke = [
+  {
+    id: 1,
+    sadrzaj: 'HTML je jednostavan',
+    datum: '2019-05-30T17:30:31.098Z',
+    vazno: true
+  },
+  {
+    id: 2,
+    sadrzaj: 'React koristi JSX sintaksu',
+    datum: '2019-05-30T18:39:34.091Z',
+    vazno: false
+  },
+  {
+    id: 3,
+    sadrzaj: 'GET i POST su najvaznije metode HTTP protokola',
+    datum: '2019-05-30T19:20:14.298Z',
+    vazno: true
+  }
+]
+
+const Poruka = ({poruka}) => {
+  return (
+    <li>{poruka.sadrzaj}</li>
+  )
+}
+
+const App = ({poruke}) => {
+  return (
+    <div>
+      <h1>Poruke</h1>
+      <ul>
+        {poruke.map(p =>
+          <Poruka key={p.id} poruka={p} />
+        )}        
+      </ul>
+    </div>
+  )
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App poruke={poruke} />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
